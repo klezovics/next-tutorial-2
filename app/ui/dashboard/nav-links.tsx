28278506290ae1c7c31faa8fcb9fea54e8c8de49
ak <klezovich.vice@gmail.com>
2024-09-09 16:33:51 +0200

    Add code

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,37 +1,44 @@
+'use client';  // This ensures that the whole component is rendered client-side
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
+} from '@heroicons/react/24/outline'; // Ensure heroicons are correctly imported
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'; // This hook is client-side only
+import clsx from 'clsx'; // Utility for conditional class names
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
+// Map of links to display in the side navigation
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
+  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
-    <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <a
-            key={link.name}
-            href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </a>
-        );
-      })}
-    </>
+      <>
+        {links.map((link) => {
+          const LinkIcon = link.icon;
+          return (
+              <Link
+                  key={link.name}
+                  href={link.href}
+                  className={clsx(
+                      'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                      {
+                        'bg-sky-100 text-blue-600': pathname === link.href,
+                      },
+                  )}
+              >
+                <LinkIcon className="w-6" />
+                <p className="hidden md:block">{link.name}</p>
+              </Link>
+          );
+        })}
+      </>
   );
 }
